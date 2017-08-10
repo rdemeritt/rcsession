949125bee95e40dc9370c8d5c28c78779b29368d
rdemeritt <rdemeritt@secureworks.com>
2017-08-10 15:44:54 -0400

    rcsession class

--- rcsession.py
+++ rcsession.py
@@ -4,69 +4,53 @@ Created by Ron DeMeritt <rdemeritt@gmail.com>
 import requests
 import json
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 
-def setup_session(_token):
-    headers = {
-        'authorization': 'TOKEN %s' % _token,
-        'content-type': 'application/json',
-        'accept': 'application/json'
-    }
+class RCSession:
 
-    session = requests.Session()
-    session.headers.update(headers)
+    token_url = 'https://redcloak.secureworks.com/token'
+    username = ''
 
-    # ensure our token is still valid
-    token = get_token(session)
+    def __init__(self, _token):
+        self.headers = {
+            'authorization': 'TOKEN %s' % _token,
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        }
 
-    if not token:
-        print('Token Expired...  Trying to renew')
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
+        self.username = self.get_token()["token"]["user_friendly_name"]
 
-        # attempt to get a new token
-        new_token = renew_token(session)
+    def get_token(self):
+        response = self.session.get(self.token_url)
 
-        if not new_token:
+        if response.status_code == 404:
             return False
 
-        # update our session headers with our new token
-        session.headers.update(
-            {'authorization': 'TOKEN %s' % new_token})
-    global user_name
-    # user_name = json.loads(
-    #     session.get(token_url).text)["token"]["user_friendly_name"]
-    # print(json.loads(session.get(token_url).text))
-    user_name = get_token(session)["token"]["user_friendly_name"]
-    return session
+        return json.loads(response.text)
 
+    def renew_token(self):
+        response = self.session.post(self.token_url)
 
-def get_token(_session):
-    response = _session.get(token_url)
+        if response.status_code != 200:
+            print("ERROR: Got %s, but expected 200" % response.status_code)
+            print(
+                'Your token has expired.  '
+                'Get a new token at %s' % self.token_url)
+            return False
 
-    if response.status_code == 404:
-        return False
+        RCSession.__init__(self, json.loads(response.text)['serialized'])
+        return True
 
-    return json.loads(response.text)
-
-
-# fetch our authentication token from a json file
-def get_token_from_file(_file_name):
-    with open(_file_name) as token_json:
-        return json.load(token_json)['token']
-
-
-def renew_token(_session):
-    response = _session.post(token_url)
-
-    if response.status_code != 200:
-        print("ERROR: Got %s, but expected 200" % response.status_code)
-        print(
-            'Your token has expired.  '
-            'Get a new token at %s' % token_url)
-        return False
-
-    return json.loads(response.text)['serialized']
-
-
-token_url = 'https://redcloak.secureworks.com/token'
-user_name = ''
+    # fetch our authentication token from a json document
+    #
+    # example below:
+    # {
+    #     "token": "3JkZW1lcml0dEBnbWFpbC5jb20QkJHtx/...."
+    #  }
+    #
+    def get_token_from_file(self, _file_name):
+        with open(_file_name) as token_json:
+            return json.load(token_json)['token']
