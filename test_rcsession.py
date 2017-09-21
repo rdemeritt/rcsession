b8a7103f61d2bc0d5a3ece440b5524644bddd794
Ron DeMeritt <rdemeritt@secureworks.com>
2017-09-21 14:54:10 +0000

    Fixed merge conflict

--- test_rcsession.py
+++ test_rcsession.py
@@ -46,7 +46,6 @@ def main():
     # dump some pages
     print("TOKEN:\n" + str(session.get_token()))
     print("\nFRIENDLY NAME:\n" + str(session.user_friendly_name))
-<<<<<<< HEAD
     print("\nCLIENT DOMAINS:\n" + str(
         dump_url_response(session, session.client_domains_url + count)))
     print("\nWATCHLISTS:\n" + str(
